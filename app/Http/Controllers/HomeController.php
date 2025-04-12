a815cecef33ff3f7c73e7ed3eff1964b58f563f6
Lacy <azhuk.ig@gmail.com>
2025-04-12 08:19:50 +0200

    First commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Auth;
+use DB;
+use View;
+
+class HomeController extends Controller
+{
+    public function index() {
+        $members = DB::table('users')->orderByDesc('rank')->where("rank", ">=", 1)->get();
+        $sponsors = DB::table('sponsors')->get();
+
+        return View::make("home")->with([
+            "members"=>$members,
+            "sponsors"=>$sponsors
+        ]);
+    }
+}
