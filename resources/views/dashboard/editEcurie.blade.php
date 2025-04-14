4a7e24385674ee7cc35e2d2ad9ef1d4180fe075f
Lacy <azhuk.ig@gmail.com>
2025-04-14 11:13:15 +0200

    Ajout du choix aucun sponsor

--- resources/views/dashboard/editEcurie.blade.php
+++ resources/views/dashboard/editEcurie.blade.php
@@ -80,8 +80,9 @@
                                   <div class="w-full max-w-full shrink-0 md:w-6/12 md:flex-0 px-3">
                                       <label for="sponsor" class="mb-2 ml-1 font-bold text-xs text-slate-700">Sponsor</label>
                                       <select required name="sponsor" class="focus:shadow-soft-primary-outline text-sm leading-5.6 ease-soft block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding px-3 py-2 font-normal text-gray-700 outline-none transition-all placeholder:text-gray-500 focus:border-fuchsia-300 focus:outline-none">
+                                        <option value="Aucun" {{ $ecurie->name == "Aucun" ? "selected" : "" }}>Aucun</option>
                                         @foreach ($sponsors as $sponsor)
-                                        <option value="{{ $sponsor->name }}" {{ $sponsor->name == $ecurie->sponsor }}>{{ $sponsor->name }}</option>
+                                        <option value="{{ $sponsor->name }}" {{ $sponsor->name == $ecurie->sponsor ? "selected" : "" }}>{{ $sponsor->name }}</option>
                                         @endforeach
                                       </select>
                                   </div>
