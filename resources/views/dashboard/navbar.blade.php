10e91425d659efe9a865ef84c8a542e495391fa6
Lacy <azhuk.ig@gmail.com>
2025-04-26 02:04:26 +0200

    Changement de la navbar

--- resources/views/dashboard/navbar.blade.php
+++ resources/views/dashboard/navbar.blade.php
@@ -10,15 +10,17 @@
           <!-- <li class="flex items-center">
             <a class="inline-block px-8 py-2 mb-0 mr-4 text-xs font-bold text-center uppercase align-middle transition-all bg-transparent border border-solid rounded-lg shadow-none cursor-pointer leading-pro border-fuchsia-500 ease-soft-in hover:scale-102 active:shadow-soft-xs text-fuchsia-500 hover:border-fuchsia-500 active:bg-fuchsia-500 active:hover:text-fuchsia-500 hover:text-fuchsia-500 tracking-tight-soft hover:bg-transparent hover:opacity-75 hover:shadow-none active:text-white active:hover:bg-transparent" target="_blank" href="https://www.creative-tim.com/builder/soft-ui?ref=navbar-dashboard&amp;_ga=2.76518741.1192788655.1647724933-1242940210.1644448053">Online Builder</a>
           </li> -->
+          @if (auth()->user()->rank >= 5)
           <li class="flex items-center">
-            <a href="./pages/sign-in.html" class="block px-0 py-2 text-sm font-semibold transition-all ease-nav-brand text-slate-500">
+            <a href="#" class="block px-0 py-2 text-sm font-semibold transition-all ease-nav-brand text-slate-500">
               <img
-                src="https://cdn.discordapp.com/avatars/{{ auth()->user()->id }}/{{ auth()->user()->avatar }}"
-                alt="Photo de profile"
+                src="{{ url('storage/profile/' . auth()->user()->id . '.png') }}"
+                alt="Photo de profil"
                 class="inline-flex items-center justify-center mr-2 text-white transition-all duration-200 ease-in-out w-9 h-9 text-sm rounded-circle"/>
                 <span class="hidden sm:inline">{{ auth()->user()->fullname }}</span>
             </a>
           </li>
+          @endif
         </ul>
       </div>
     </div>
