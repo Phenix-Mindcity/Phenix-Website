d644a69071d7aa41cc3590c88169d7e598cb06dd
Lacy <azhuk.ig@gmail.com>
2025-04-27 20:47:11 +0200

    Patch du visuel des résultats

--- resources/views/dashboard/result.blade.php
+++ resources/views/dashboard/result.blade.php
@@ -98,6 +98,7 @@
                                                     </select>
                                                 </td>
                                             </tr>
+                                            @endforeach
                                             <tr>
                                                 <td class="p-2 text-sm leading-normal text-center align-middle bg-transparent border-b whitespace-nowrap">
                                                     <span class="text-xs font-semibold leading-tight">Valider les résultats ?</span>
@@ -109,7 +110,6 @@
                                                     <button type="submit" class="inline-block px-4 py-2 font-bold text-center text-white uppercase align-middle transition-all rounded-lg cursor-pointer bg-gradient-to-tl from-purple-700 to-pink-500 leading-pro text-xs ease-soft-in tracking-tight-soft shadow-soft-md bg-150 bg-x-25 hover:scale-102 active:opacity-85 hover:shadow-soft-xs">Valider</button>
                                                 </td>
                                             </tr>
-                                            @endforeach
                                         </form>
                                     </tbody>
                                 </table>
