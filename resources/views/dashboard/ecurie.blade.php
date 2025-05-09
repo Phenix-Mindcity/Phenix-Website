7f698e4febf04d7915d8612ded9bba05d864b8d7
Lacy <imlacy.ig@gmail.com>
2025-05-10 00:57:05 +0200

    Ajout de l'ID des écuries pour la compta

--- resources/views/dashboard/ecurie.blade.php
+++ resources/views/dashboard/ecurie.blade.php
@@ -97,7 +97,7 @@
                       <h3 class="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                           {{ $ecurie->name }}
                       </h3>
-                    <p>{{ $ecurie->sponsor }}</p>
+                    <p>ID : {{ $ecurie->id }} <br> Sponsor : {{ $ecurie->sponsor }}</p>
                     <a href="/editEcurie/{{ $ecurie->id }}">
                         <button type="button" class="inline-block px-4 py-2 font-bold text-center text-white uppercase align-middle transition-all rounded-lg cursor-pointer bg-gradient-to-tl from-purple-700 to-pink-500 leading-pro text-xs ease-soft-in tracking-tight-soft shadow-soft-md bg-150 bg-x-25 hover:scale-102 active:opacity-85 hover:shadow-soft-xs">Modifier</button>
                     </a>
